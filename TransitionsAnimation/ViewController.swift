cbc020d4e51a94a5a5c95fda8b941ad2b4da1086
Victor Hong <victorhong1988@gmail.com>
2016-08-03 16:31:45 -0400

    Initialize

---
+++ TransitionsAnimation/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  TransitionsAnimation
+//
+//  Created by Victor Hong on 7/11/16.
+//  Copyright © 2016 Victor Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let transitionManager = TransitionManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func unwindToViewController(sender: UIStoryboardSegue) {
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        // this gets a reference to the screen that we're about to transition to
+        let toViewController = segue.destinationViewController as UIViewController
+        
+        // instead of using the default transition animation, we'll ask
+        // the segue to use our custom TransitionManager object to manage the transition animation
+        toViewController.transitioningDelegate = self.transitionManager
+        
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return self.presentingViewController == nil ? UIStatusBarStyle.Default : UIStatusBarStyle.LightContent
+    }
+}
+
